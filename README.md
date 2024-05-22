4c4eff73292d2fc7db34327ae2badf0cc5b27816
John Browning <john@darkstar.jeb.dev>
2024-05-22 10:58:01 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# vmwaretoocpv
+# vmwaretooocpv
+
+(alpha readme - 5-22-24 )
+
+## What is it?
+A small Python Pandas script to analyze RRV Tool output and (eventually) make architecture suggestions for creating a 1:1 OpenShift Virtualization environment.  This will assist in planning a new environment to migrate Virtual Machines from VSphere over to OpenShift Virtualization.
+
+## What does it do?
+Takes standard CSV from RRV Tool Output and summarizes it, generates summary data in PDF.
+
+## Planned features
+Output suggested architecture for Red Hat OpenShift Virtualization Clusters based on existing VMWare cluster data.
+
+## Instructions
+Within the repo directory (assuming you've git cloned this repo)
+
+1. Create a python 3 venv
+```
+python3 -m venv venv
+```
+
+2. Source new env
+
+```
+source venv/bin/activate
+```
+
+3. Install requirements to venv
+
+```
+pip install -r requirements.txt
+```
+
+4. Save RRV Output
+```
+Save "sheet2" from RRV Tools output as CSV named "vsphere-hosts.csv" within repo dir.
+```
+
+5. Run The Python Script
+
+```
+./analyze.py
+```
+
+6. Review text output and the newly generated analysis.pdf file
---
+++ analyze.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+if __name__ == '__main__':
+    hosts = pd.read_csv("vsphere-hosts.csv")
+    formattedVMs = pd.DataFrame(hosts)
+
+
+    #print(pd.DataFrame(hosts))
+    #print(hosts.dtypes)
+    #create a column of memory in GB
+    hosts["vHostMemInGB"] = hosts["vHostMemorySize"] / 1000
+    print("\n Analysing ", hosts["vHostCluster"].nunique(), " clusters")
+    print("\nHosts per cluster:\n")
+    print(hosts["vHostCluster"].value_counts())
+    print("Total Hosts:",hosts["vHostName"].value_counts().sum())
+    print("\n Cores Per Cluster:\n")
+    print(hosts.groupby("vHostCluster")['vHostNumCpuCores'].sum())
+    print("\n Memory(in GB) Per Cluster:\n")
+    print(hosts.groupby("vHostCluster")['vHostMemInGB'].sum())
+
+
+#plots/graphs to a PDF
+
+    with PdfPages('analysis.pdf') as pdf:
+        #hosts per cluster
+        hostfig = hosts["vHostCluster"].value_counts().plot(kind='bar', figsize=(20, 25), xlabel="Clusters", ylabel="Hosts", title="Hosts Per Cluster").get_figure()
+        pdf.savefig(hostfig)
+        #cpus per cluster
+        cpufig = hosts.groupby("vHostCluster")['vHostNumCpuCores'].sum().plot(kind='bar', figsize=(20, 25), xlabel="Clusters", ylabel="Total Cores", title="Cores Per Cluster").get_figure()
+        pdf.savefig(cpufig)
+        #memory per cluster
+        memfig = hosts.groupby("vHostCluster")['vHostMemInGB'].sum().plot(kind='bar', figsize=(20, 25), xlabel="Clusters", ylabel="Memory in GB", title="Memory (in GB) Per Cluster").get_figure()
+        pdf.savefig(memfig)
+
